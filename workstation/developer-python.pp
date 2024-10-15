84914e58d4ec543bf193cc7f6541acc3c729da68
Adam Retter <adam.retter@googlemail.com>
2024-10-15 18:20:55 +0200

    Fix permissions on installing Miniconda 3

--- workstation/developer-python.pp
+++ workstation/developer-python.pp
@@ -13,12 +13,14 @@ package { 'python3-pip':
 
 exec { 'download-miniconda3':
   command => '/usr/bin/curl -L https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -o /tmp/Miniconda3-latest-Linux-x86_64.sh',
+  user    => 'root',
   unless  => '/usr/bin/test -d /opt/miniconda',
   require => Package['curl'],
 }
 
 exec { 'install-miniconda3':
   command  => '/tmp/Miniconda3-latest-Linux-x86_64.sh -b -p /opt/miniconda',
+  user     => 'root',
   provider => 'shell',
   unless   => '/usr/bin/test -d /opt/miniconda',
   require  => Exec['download-miniconda3'],
