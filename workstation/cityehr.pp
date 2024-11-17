64fa1e88248a5da249da30955127871e138f440a
Adam Retter <adam.retter@googlemail.com>
2024-11-17 14:33:45 +0100

    Link to the latest cityEHR documentation online instead of downloading a PDF version

--- workstation/cityehr.pp
+++ workstation/cityehr.pp
@@ -21,18 +21,65 @@ exec { 'download-cityehr':
   ],
 }
 
-exec { 'download-cityehr-quickstart-guide':
-  command => "curl -L https://static.evolvedbinary.com/cityehr/${cityehr_quickstart} -o /home/${custom_user}/Desktop/cityEHR_QuickStart.pdf",
+# Add a Desktop Shortcut to the cityEHR Documentation
+exec { 'download-cityehr-logo':
+  command => "wget -O /home/${custom_user}/.local/share/icons/cityehr-logo.png https://cityehr.github.io/cityehr-documentation/images/cityehr-logo.png",
   path    => '/usr/bin',
+  creates => "/home/${custom_user}/.local/share/icons/cityehr-logo.png",
   user    => $custom_user,
-  group   => $custom_user,
-  creates => "/home/${custom_user}/Desktop/cityEHR_QuickStart.pdf",
   require => [
-    Package['file'],
-    Package['curl'],
-    File['custom_user_desktop_folder'],
+    File['local-icons'],
+    Package['wget'],
   ],
 }
+
+$cityehr_documentation_desktop_shortcut =  @("CITYEHR_DOCUMENTATION_SHORTCUT_EOF"/L)
+  [Desktop Entry]
+  Version=1.0
+  Name=cityEHR Documentation
+  Exec=/usr/bin/google-chrome-stable https://cityehr.github.io/cityehr-documentation/
+  StartupNotify=true
+  Terminal=false
+  Icon=/home/${custom_user}/.local/share/icons/cityehr-logo.png
+  Type=Application
+  | CITYEHR_DOCUMENTATION_SHORTCUT_EOF
+
+file { 'cityehr-documentation-desktop-shortcut':
+  ensure  => file,
+  path    => "/home/${custom_user}/Desktop/cityehr-documentation.desktop",
+  content => $cityehr_documentation_desktop_shortcut,
+  owner   => $custom_user,
+  group   => $custom_user,
+  mode    => '0644',
+  require => [
+    Package['desktop'],
+    File['custom_user_desktop_folder'],
+    Package['google-chrome-stable'],
+    Exec['download-cityehr-logo'],
+  ],
+}
+
+exec { 'gvfs-trust-cityehr-documentation-desktop-shortcut':
+  command     => "/usr/bin/dbus-launch gio set /home/${custom_user}/Desktop/cityehr-documentation.desktop metadata::trusted true",
+  unless      => "/usr/bin/dbus-launch gio info --attributes=metadata::trusted /home/${custom_user}/Desktop/cityehr-documentation.desktop | /usr/bin/grep trusted",
+  user        => $custom_user,
+  environment => [
+    'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus',
+  ],
+  require     => File['cityehr-documentation-desktop-shortcut'],
+}
+
+# ini_setting { 'cityehr-documentation-desktop-shortcut-position':
+#   ensure  => present,
+#   path    => "/home/${custom_user}/.config/pcmanfm-qt/lxqt/desktop-items-0.conf",
+#   section => 'cityehr-documentation.desktop',
+#   setting => 'pos',
+#   value   => '@Point(393 138)',
+#   require => [
+#     File['desktop-items-0'],
+#     File['cityehr-documentation-desktop-shortcut'],
+#   ],
+# }
 
 # Set homepage for cityEHR
 
